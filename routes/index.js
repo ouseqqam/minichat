d9e9bd6721f79c784e4de6424af6e86ecc2b109d
unknown <oussamaseqqam1303@gmail.com>
2021-06-23 20:37:27 +0100

    init

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+})
+
+router.post('/new', function(req, res) {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date()
+  });
+  res.redirect('/');
+})
+
+module.exports = router;
